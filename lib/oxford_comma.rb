cc40c15460177d290dc2aa9d01f030f1f8f77de7
Leizl Samano <leizl.samano@gmail.com>
2018-12-02 21:44:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return array.join(" and ")
+  end
+  sentence = ""
+  array.each_with_index do |name, index|
+    case array[index]
+    when array[0]
+      sentence << "#{name}"
+    when array[-1]
+      sentence << ", and #{name}"
+    else
+      sentence << ", #{name}"
+    end
+  end
+    sentence
 end
